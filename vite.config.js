b9cd2243543620f56f1fc2b95239a306c2bf9959
ridhav30 <ridhav30@gmail.com>
2025-02-10 09:42:32 +0530

    updated

--- vite.config.js
+++ vite.config.js
@@ -1,34 +1,32 @@
-import { dirname, resolve } from 'node:path';
-import { fileURLToPath } from 'node:url';
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-
+import { dirname, resolve } from 'node:path'
+import { fileURLToPath } from 'node:url'
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 const __dirname = dirname(fileURLToPath(import.meta.url));
 
+// https://vite.dev/config/
 export default defineConfig({
   build: {
     lib: {
       entry: resolve(__dirname, 'src/index.js'),
       name: 'package-example',
-      fileName: (format) => {
-        if (format === 'es') {
-          return 'package-example.js';
-        } else if (format === 'umd') {
-          return 'package-example.umd.cjs';
-        }
-      },
-      formats: ['es', 'umd'], // Include both ESM and UMD formats
+      // the proper extensions will be added
+      fileName: 'package-example',
+      formats: ["es"]
     },
-    rollupOptions: {
-      // Externalize React and ReactDOM
-      external: ['react', 'react-dom'],
-      output: {
-        globals: {
-          react: 'React',
-          'react-dom': 'ReactDOM',
-        },
+  },
+
+  rollupOptions: {
+    // make sure to externalize deps that shouldn't be bundled
+    // into your library
+    external: ['react'],
+    output: {
+      // Provide global variables to use in the UMD build
+      // for externalized deps
+      globals: {
+        react: 'React',
       },
     },
   },
-  plugins: [react()],
-});
+  plugins: [react()]
+})
